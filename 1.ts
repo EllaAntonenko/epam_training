88903523a732676f64abdd84316dd98493137a90
Elvira Antonenko <109676457+EllaAntonenko@users.noreply.github.com>
2022-08-30 10:53:44 +0300

    Delete 1.ts

--- 1.ts
+++
@@ -1,6 +0,0 @@
-// 1. Given an array consisting of movie names, iterate over the array with the output of the names of each movie to the console
-
-const films = ['Avatar', 'Titanic', 'Frozen']
-    films.forEach(function(elem){
-        console.log(elem)
-})
